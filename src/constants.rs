d69e15ce1d91a5d6743e6640c7fdcc7f4a841160
Harry-kp <chaudharyharshit9@gmail.com>
2025-12-11 14:27:30 +0530

    chore: trigger auto-docs workflow

--- src/constants.rs
+++ src/constants.rs
@@ -3,7 +3,7 @@ pub const GITHUB_REPO: &str = "mercury";
 #[allow(dead_code)]
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 #[allow(dead_code)]
-pub const APP_NAME: &str = "Mercury";
+pub const APP_NAME: &str = "Mercury API Client";
 
 pub fn get_repo_url() -> String {
     format!("https://github.com/{}/{}", GITHUB_USERNAME, GITHUB_REPO)
